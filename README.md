dd3c51aaff847afe724df62c207b40c8273aade8
cryptordx <cryptordx@mokshya.io>
2022-11-03 09:12:26 +0545

    readme

--- README.md
+++ README.md
@@ -1,2 +1,97 @@
-# aptos-token-staking
-The smart contract provides staking for Tokens and NFTs. The creators can decide the APR and way to distribute the gains.
+Candy machine smart contract for Aptos Blockchain.
+
+# Deploy contract
+
+```
+aptos init
+
+```
+# Test
+
+``` 
+aptos move test 
+```
+# Publish
+
+```
+ aptos move publish --named-addresses mokshyastaking::tokenstaking=your_address
+```
+Copy the account address and replace in tests/staking.ts
+```javascript
+import {AptosClient, AptosAccount, FaucetClient, TxnBuilderTypes} from "aptos";
+
+#reate Staking
+
+const NODE_URL = "https://fullnode.devnet.aptoslabs.com";
+const FAUCET_URL = "https://faucet.devnet.aptoslabs.com";
+// Creator Account
+const account1 = new AptosAccount();
+// Staker Account
+const account2 = new AptosAccount();
+const create_staking_payloads = {
+      type: "entry_function_payload",
+      function: "0xf40bb98bf343e6c091af235e2e4b1953b38919f266ce10a091dfeae8b06f3f9e::tokenstaking::create_staking",
+      type_arguments: ["0x1::aptos_coin::AptosCoin"],
+      arguments: [BigInt(1),collection,BigInt(10)
+      ],
+    };
+    let txnRequest = await client.generateTransaction(account1.address(), create_staking_payloads);
+    let bcsTxn = AptosClient.generateBCSTransaction(account1, txnRequest);
+    await client.submitSignedBCSTransaction(bcsTxn);
+
+```
+
+# Create Whitelist
+```javascript
+const whitelist_list = [] //add all the addresses you want to whitelist.
+const create_whitelist_payloads = {
+  type: "entry_function_payload",
+  function: pid+"::candymachine::create_whitelist",
+  type_arguments: [],
+  arguments: [getresourceAccount['changes'][2]['address'],whitelist_list,any_random_number],
+};
+txnRequest = await client.generateTransaction(alice.address(), create_whitelist_payloads);
+bcsTxn = AptosClient.generateBCSTransaction(alice, txnRequest);
+transactionRes = await client.submitSignedBCSTransaction(bcsTxn);
+console.log("Whitelist created: "+transactionRes.hash)
+```
+# Stake Token
+```javascript
+ const create_staking_payloads = {
+      type: "entry_function_payload",
+      function: "0xf40bb98bf343e6c091af235e2e4b1953b38919f266ce10a091dfeae8b06f3f9e::tokenstaking::stake_token",
+      type_arguments: [],
+      arguments: [account1.address(),collection,tokenname,tokenPropertyVersion,BigInt(1)
+      ],
+    };
+    let txnRequest = await client.generateTransaction(account2.address(), create_staking_payloads);
+    let bcsTxn = AptosClient.generateBCSTransaction(account2, txnRequest);
+    await client.submitSignedBCSTransaction(bcsTxn);
+```
+# Claim Reward
+```javascript
+ const create_staking_payloads = {
+      type: "entry_function_payload",
+      function: "0xf40bb98bf343e6c091af235e2e4b1953b38919f266ce10a091dfeae8b06f3f9e::tokenstaking::claim_reward",
+      type_arguments: ["0x1::aptos_coin::AptosCoin"],
+      arguments: [collection,tokenname,account1.address()
+      ],
+    };
+    let txnRequest = await client.generateTransaction(account2.address(), create_staking_payloads);
+    let bcsTxn = AptosClient.generateBCSTransaction(account2, txnRequest);
+    await client.submitSignedBCSTransaction(bcsTxn);
+```
+# Unstake Token
+```javascript
+const create_staking_payloads = {
+      type: "entry_function_payload",
+      function: "0xf40bb98bf343e6c091af235e2e4b1953b38919f266ce10a091dfeae8b06f3f9e::tokenstaking::unstake_token",
+      type_arguments: ["0x1::aptos_coin::AptosCoin"],
+      arguments: [account1.address(),collection,tokenname,tokenPropertyVersion,1
+      ],
+    };
+    let txnRequest = await client.generateTransaction(account2.address(), create_staking_payloads);
+    let bcsTxn = AptosClient.generateBCSTransaction(account2, txnRequest);
+    await client.submitSignedBCSTransaction(bcsTxn);
+
+```
---
+++ tests/staking.ts
@@ -0,0 +1,137 @@
+
+import { AptosClient,TokenClient, AptosAccount, FaucetClient, } from "aptos";
+
+
+const NODE_URL = process.env.APTOS_NODE_URL || "https://fullnode.devnet.aptoslabs.com";
+const FAUCET_URL = process.env.APTOS_FAUCET_URL || "https://faucet.devnet.aptoslabs.com";
+
+
+const client = new AptosClient(NODE_URL);
+const faucetClient = new FaucetClient(NODE_URL, FAUCET_URL);
+// Creator Account
+const account1 = new AptosAccount();
+// Staker Account
+const account2 = new AptosAccount();
+const collection = "Mokshya Collection";
+const tokenname = "Mokshya Token #1";
+const description="Mokshya Token for test"
+const uri = "https://github.com/mokshyaprotocol"
+const tokenPropertyVersion = BigInt(0);
+
+const token_data_id =  {creator: account1.address().hex(),
+  collection: collection,
+  name: tokenname,
+
+}
+const tokenId = {
+  token_data_id,
+  property_version: `${tokenPropertyVersion}`,
+};
+const tokenClient = new TokenClient(client); // <:!:section_1b
+
+/**
+ * Testing Staking Contract
+ */
+ describe("Token Staking", () => {
+  it ("Create Collection", async () => {
+    await faucetClient.fundAccount(account1.address(), 1000000000);//Airdropping
+    const create_collection_payloads = {
+      type: "entry_function_payload",
+      function: "0x3::token::create_collection_script",
+      type_arguments: [],
+      arguments: [collection,description,uri,BigInt(100),[false, false, false]],
+    };
+    let txnRequest = await client.generateTransaction(account1.address(), create_collection_payloads);
+    let bcsTxn = AptosClient.generateBCSTransaction(account1, txnRequest);
+    await client.submitSignedBCSTransaction(bcsTxn);
+
+  });
+  it ("Create Token", async () => {
+    const create_token_payloads = {
+      type: "entry_function_payload",
+      function: "0x3::token::create_token_script",
+      type_arguments: [],
+      arguments: [collection,tokenname,description,BigInt(5),BigInt(10),uri,account1.address(),
+        BigInt(100),BigInt(0),[ false, false, false, false, false, false ],
+        [ "attack", "num_of_use"],
+        [[1,2],[1,2]],
+        ["Bro","Ho"]
+      ],
+    };
+    let txnRequest = await client.generateTransaction(account1.address(), create_token_payloads);
+    let bcsTxn = AptosClient.generateBCSTransaction(account1, txnRequest);
+    await client.submitSignedBCSTransaction(bcsTxn);
+  });
+  it ("Opt In Transfer ", async () => {
+    await faucetClient.fundAccount(account2.address(), 1000000000);//Airdropping
+    const create_token_payloads = {
+      type: "entry_function_payload",
+      function: "0x3::token::opt_in_direct_transfer",
+      type_arguments: [],
+      arguments: [true],
+    };
+    let txnRequest = await client.generateTransaction(account2.address(), create_token_payloads);
+    let bcsTxn = AptosClient.generateBCSTransaction(account2, txnRequest);
+    await client.submitSignedBCSTransaction(bcsTxn);
+  });
+  it ("Transfer Token ", async () => {
+    const create_token_payloads = {
+      type: "entry_function_payload",
+      function: "0x3::token::transfer_with_opt_in",
+      type_arguments: [],
+      arguments: [account1.address(),collection,tokenname,tokenPropertyVersion,account2.address(),BigInt(1)],
+    };
+    let txnRequest = await client.generateTransaction(account1.address(), create_token_payloads);
+    let bcsTxn = AptosClient.generateBCSTransaction(account1, txnRequest);
+    await client.submitSignedBCSTransaction(bcsTxn);
+  
+  });
+  it ("Create Staking", async () => {
+    const create_staking_payloads = {
+      type: "entry_function_payload",
+      function: "0xf40bb98bf343e6c091af235e2e4b1953b38919f266ce10a091dfeae8b06f3f9e::tokenstaking::create_staking",
+      type_arguments: ["0x1::aptos_coin::AptosCoin"],
+      arguments: [BigInt(1),collection,BigInt(10)
+      ],
+    };
+    let txnRequest = await client.generateTransaction(account1.address(), create_staking_payloads);
+    let bcsTxn = AptosClient.generateBCSTransaction(account1, txnRequest);
+    await client.submitSignedBCSTransaction(bcsTxn);
+  });
+  it ("Stake Token", async () => {
+    const create_staking_payloads = {
+      type: "entry_function_payload",
+      function: "0xf40bb98bf343e6c091af235e2e4b1953b38919f266ce10a091dfeae8b06f3f9e::tokenstaking::stake_token",
+      type_arguments: [],
+      arguments: [account1.address(),collection,tokenname,tokenPropertyVersion,BigInt(1)
+      ],
+    };
+    let txnRequest = await client.generateTransaction(account2.address(), create_staking_payloads);
+    let bcsTxn = AptosClient.generateBCSTransaction(account2, txnRequest);
+    await client.submitSignedBCSTransaction(bcsTxn);
+  });
+  it ("Get Reward", async () => {
+    const create_staking_payloads = {
+      type: "entry_function_payload",
+      function: "0xf40bb98bf343e6c091af235e2e4b1953b38919f266ce10a091dfeae8b06f3f9e::tokenstaking::claim_reward",
+      type_arguments: ["0x1::aptos_coin::AptosCoin"],
+      arguments: [collection,tokenname,account1.address()
+      ],
+    };
+    let txnRequest = await client.generateTransaction(account2.address(), create_staking_payloads);
+    let bcsTxn = AptosClient.generateBCSTransaction(account2, txnRequest);
+    await client.submitSignedBCSTransaction(bcsTxn);
+  });
+  it ("Unstake Token", async () => {
+    const create_staking_payloads = {
+      type: "entry_function_payload",
+      function: "0xf40bb98bf343e6c091af235e2e4b1953b38919f266ce10a091dfeae8b06f3f9e::tokenstaking::unstake_token",
+      type_arguments: ["0x1::aptos_coin::AptosCoin"],
+      arguments: [account1.address(),collection,tokenname,tokenPropertyVersion,1
+      ],
+    };
+    let txnRequest = await client.generateTransaction(account2.address(), create_staking_payloads);
+    let bcsTxn = AptosClient.generateBCSTransaction(account2, txnRequest);
+    await client.submitSignedBCSTransaction(bcsTxn);
+  });
+  });
